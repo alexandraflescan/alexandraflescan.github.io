d96727518e03aad2f8558850b691e3951bb33bd0
constantinserediuc <serediuc.constantin@info.uaic.ro>
2020-04-25 12:13:49 +0300

    added touch events for mobile

--- app.js
+++ app.js
@@ -4,86 +4,84 @@ var hole = document.getElementById("hole");
 
 var movingBallj = $("#movingBall");
 var holej = $("#hole");
-dragElement(movingBall);
 
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id)) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-    var t = is_colliding($("#" + elmnt.id),holej) // should return whether they are touching
-    if (t) {
-        elmnt.style.top = "20%";
-        elmnt.style.left = "30%";
-        alert("Ai castigat!")
-    }
-  }
+ var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+ if (elmnt) {
+   /* if present, the header is where you move the DIV from:*/
+   elmnt.on("mousedown touchstart", (event) => dragMouseDown())
+   }
 
 
-  function touching(d1,d2){
-    let ox = Math.abs(d1.x - d2.x) < (d1.x < d2.x ? d2.width : d1.width);
-    let oy = Math.abs(d1.y - d2.y) < (d1.y < d2.y ? d2.height : d1.height);
-    return ox && oy;
-}
+ function dragMouseDown(e) {
+   e = e || window.event;
+  e.preventDefault();
+   // get the mouse cursor position at startup:
+   pos3 = e.clientX;
+   pos4 = e.clientY;
+   $(document).on("mouseup touchend", (event) => {
+       console.log("touchend")
+       $(document).off("mouseup touchend");
+       $(document).off("mousemove touchmove"); 
+       closeDragElement();
+   } );
+   // call a function whenever the cursor moves:
+  
+    movingBall.addEventListener('touchmove',elementDrag, false);
+  
 
+   //$(document).on("mousemove touchmove", (e) => elementDrag());
+ 
+ }
+
+ function elementDrag(e) {
+  var e = event.changedTouches[0];
+   // calculate the new cursor position:
+   pos1 = pos3 - e.clientX;
+   pos2 = pos4 - e.clientY;
+   pos3 = e.clientX;
+   pos4 = e.clientY;
+   var elementOffSet = document.getElementById(elmnt[0].id);
+   elementOffSet.style.top = (elementOffSet.offsetTop - pos2) + "px";
+   elementOffSet.style.left = (elementOffSet.offsetLeft - pos1) + "px";
+ }
+
+ function closeDragElement() {
+   console.log("in close drag element");
+   /* stop moving when mouse button is released:*/
+   var t = is_colliding(elmnt, holej) // should return whether they are touching
+   if (t) {
+       elmnt.css("top", "20%");
+       elmnt.css("left", "30%");
+       alert("Ai castigat!")
+   }
+ }
 
 }
+
+  dragElement(movingBallj);
 
 
 
 
 var is_colliding = function( $div1, $div2 ) {
-	// Div 1 data
-	var d1_offset             = $div1.offset();
-	var d1_height             = $div1.outerHeight( true );
-	var d1_width              = $div1.outerWidth( true );
-	var d1_distance_from_top  = d1_offset.top + d1_height;
-	var d1_distance_from_left = d1_offset.left + d1_width;
+ // Div 1 data
+ var d1_offset             = $div1.offset();
+ var d1_height             = $div1.outerHeight( true );
+ var d1_width              = $div1.outerWidth( true );
+ var d1_distance_from_top  = d1_offset.top + d1_height;
+ var d1_distance_from_left = d1_offset.left + d1_width;
 
-	// Div 2 data
-	var d2_offset             = $div2.offset();
-	var d2_height             = $div2.outerHeight( true );
-	var d2_width              = $div2.outerWidth( true );
-	var d2_distance_from_top  = d2_offset.top + d2_height;
-	var d2_distance_from_left = d2_offset.left + d2_width;
+ // Div 2 data
+ var d2_offset             = $div2.offset();
+ var d2_height             = $div2.outerHeight( true );
+ var d2_width              = $div2.outerWidth( true );
+ var d2_distance_from_top  = d2_offset.top + d2_height;
+ var d2_distance_from_left = d2_offset.left + d2_width;
 
-	var not_colliding = ( d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left );
+ var not_colliding = ( d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left );
 
-	// Return whether it IS colliding
-	return ! not_colliding;
+ // Return whether it IS colliding
+ return ! not_colliding;
 };
